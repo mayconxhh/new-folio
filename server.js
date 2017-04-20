09e69a63ec9ce4140d685bbc72aa6c1b0dbf8793
mayconxhh <rx_reycel96@gmail.com>
2017-04-19 22:31:32 -0500

    se agrego estilos a vista cv y contacto

--- server.js
+++ server.js
@@ -5,19 +5,20 @@ var morgan = require('morgan')
 var mongoose = require('mongoose')
 var port = process.env.PORT || 5000
 var routes = require('./routes')
-var routesAdmin = require('./routes/admin')
+// var routesAdmin = require('./routes/admin')
+// var config = require('./config')
 
 app.engine('html', require('ejs').renderFile)
 
 // Database configuration
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/portafolio', function(err){
-	if (err) {
-		console.log('Ocurrió un error al tratar de conectar con la base de datos.')
-	} else {
-		console.log('Se conecto con la base de datos')
-	}
-})
+// mongoose.Promise = global.Promise
+// mongoose.connect(config.mlab, function(err){
+// 	if (err) {
+// 		console.log('Ocurrió un error al tratar de conectar con la base de datos.')
+// 	} else {
+// 		console.log('Se conecto con la base de datos')
+// 	}
+// })
 
 app.set('views', __dirname+'/views')
 app.set('view engine', 'html')
@@ -29,7 +30,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', routes)
-app.use('/admin', routesAdmin)
+// app.use('/admin', routesAdmin)
 
 // app.get('*', function(req, res){
 // 	res.render('index')
